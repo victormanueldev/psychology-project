9cdf49e4c2a6f3ac04cef6f811d9bb3e5b15c52b
Victor Manuel <victormalsx@gmail.com>
2020-07-22 22:21:19 -0500

    Design init

---
+++ frontend/js/app.js
@@ -0,0 +1,11 @@
+let nombre = document.getElementById('nombre');
+let aplleido
+let 
+
+function guardar(nombre, aplleido, edada) {
+    enviar()
+}
+
+function enviar(params) {
+    //CoNECT WITH THE SERVER
+}
