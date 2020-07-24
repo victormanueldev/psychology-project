433db24675bfa67a05826c1f46d026f5bc4b9303
Victor Arenas <victor.arenas@digitalamericas.ai>
2020-07-23 20:19:38 -0500

    Creamos la estructura para enviar los datos al servidor

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,11 +1,47 @@
-let nombre = document.getElementById('nombre');
-let aplleido
-let 
+// Captura de datos desde el HTML 
+let nombre          = document.getElementById('nombre').value;
+let apellido        = document.getElementById('apellido').value;
+let numDoc          = document.getElementById('num-doc').value;
+let edad            = document.getElementById('edad').value;
+let telefono        = document.getElementById('telefono').value;
+let email           = document.getElementById('email').value;
+let fechaConsulta   = document.getElementById('fecha-consulta').value;
+let horaConsulta    = document.getElementById('hora-consulta').value;
+let motivo          = document.getElementById('motivo').value;
 
-function guardar(nombre, aplleido, edada) {
-    enviar()
+/**
+ * Agrupa los valores de los campos de texto del HTML
+ * en un objeto JSON
+ */
+function clickButton() {
+
+    const consulta = {
+        nombre : nombre,
+        apellido,
+        numDoc,
+        edad,
+        telefono,
+        email,
+        fechaConsulta,
+        horaConsulta,
+        motivo,
+    }
+
+    sendToServer(consulta)
 }
 
-function enviar(params) {
-    //CoNECT WITH THE SERVER
+/**
+ * RESTFUL
+ */
+function sendToServer(dataAEnviar) {
+    axios({
+        method  : 'POST', // REQUEST
+        url : "http://localhost:5000/agendar-cita",
+        data : {
+            dataAEnviar
+        }
+    })
+    .then( (respuesta) => { // RESPONSE
+        console.log(respuesta.data)
+    })
 }
